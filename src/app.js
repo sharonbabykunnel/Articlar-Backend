8e41a1297689a594cc42d0f721db260e55f0c06f
Sharon <sharonkunnel2003@gmail.com>
2024-09-25 20:44:12 +0530

    25.09.4

--- src/app.js
+++ src/app.js
@@ -16,6 +16,9 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use('/', (req, res) => {
+  res.send('hi')
+})
 app.use(`/api/auth`, authRoutes);
 app.use(`/api/home/`, homeRoutes);
 app.use(`/api/profile/`, profileRoutes);
